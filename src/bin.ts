1788775b2f7025723d08dfe49bb9ffda88fed6d4
Raine Revere <raine@cybersemics.org>
2022-06-07 10:29:15 -0600

    Do nothing when console.logs have already been removed.

--- src/bin.ts
+++ src/bin.ts
@@ -4,22 +4,35 @@ import { execa } from 'execa'
 import chalk from 'chalk'
 
 // run git diff
-const { stdout: diff } = await execa('git', ['diff', '--staged'])
+const { stdout: unstaged } = await execa('git', ['diff'])
+const { stdout: staged } = await execa('git', ['diff', '--staged'])
 
-if (!diff) {
-  const { stdout: unstaged } = await execa('git', ['diff'])
+if (!staged) {
   console.info(`No staged changes`)
-  if (unstaged) {
-    console.info(
-      `\nYou do have unstaged changes. ${chalk.cyan('rm-diff-consoles')} only works on staged changes. Use ${chalk.cyan(
-        'git add -A',
-      )} to stage all changes and then re-run ${chalk.cyan('rm-diff-consoles')}.`,
-    )
+}
+
+if (unstaged) {
+  if (!staged) console.info('')
+
+  // if the only unstaged changes are removed console.logs, then this is a redundant call and we can do nothing
+  const changes = unstaged.match(/^[+-][^+-][^+-].*/gm)
+  const isConsolesOnly = changes?.every(match => /^-.*console\.log/.test(match))
+  if (isConsolesOnly) {
+    process.exit(0)
   }
+
+  console.info(
+    `You have unstaged changes. ${chalk.cyan('rm-diff-consoles')} only works on staged changes. Use ${chalk.cyan(
+      'git add -A',
+    )} to stage all changes and then re-run ${chalk.cyan('rm-diff-consoles')}.`,
+  )
+}
+
+if (!staged || unstaged) {
   process.exit(0)
 }
 
-const results = await rmDiffConsoles(process.cwd(), diff)
+const results = await rmDiffConsoles(process.cwd(), staged)
 
 console.info(
   results.length > 0
